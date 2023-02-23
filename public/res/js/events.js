9bcd4e9dafccfe019499c93974b7915fb74ae96e
Pascal Wittler <git@pascalwittler.de>
2023-02-23 01:19:23 +0100

    insert text + links to events table via javascript

--- public/res/js/events.js
+++ public/res/js/events.js
@@ -1,14 +1,27 @@
 document.addEventListener('DOMContentLoaded', function() {
+  function createTableCellContent(text, link) {
+    if (link === null) {
+      return text;
+    }
+
+    return `<a href="${link}" target="_blank" rel="noreferrer noopener">${text}</a>`;
+  }
+
   function insertEvents(events) {
     const eventsTable = document.querySelector('.events tbody');
 
     events.forEach(event => {
+      const dateMarkup = createTableCellContent(event.date.text, event.date.link);
+      const timeMarkup = createTableCellContent(event.time.text, event.time.link);
+      const locationMarkup = createTableCellContent(event.location.text, event.location.link);
+      const titleMarkup = createTableCellContent(event.title.text, event.title.link);
+
       eventsTable.innerHTML += `
         <tr>
-          <td>${event.date}</td>
-          <td>${event.time}</td>
-          <td>${event.location}</td>
-          <td>${event.title}</td>
+          <td>${dateMarkup}</td>
+          <td>${timeMarkup}</td>
+          <td>${locationMarkup}</td>
+          <td>${titleMarkup}</td>
         </tr>
       `;
     });
