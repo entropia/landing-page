78235ee02135f71aa543d10ce7242d67333f062d
Jens Hausdorf <mail@jens-hausdorf.de>
2024-01-10 19:25:13 +0100

    Now it should work in PHP 7.4

--- public/api/entropia-cloud-events-json-api.php
+++ public/api/entropia-cloud-events-json-api.php
@@ -6,7 +6,6 @@ require '../../vendor/autoload.php';
 
 class EntropiaCloudEventsJsonApi
 {
-    const TIMEZONE = new DateTimeZone('Europe/Berlin');
     const DEFAULT_ICS_URL = 'https://cloud.entropia.de/remote.php/dav/public-calendars/3Ag5YobrwKpWYKsZ?export';
     const DEFAULT_MAX_ENTRIES = 10;
 
@@ -21,22 +20,23 @@ class EntropiaCloudEventsJsonApi
 
     private static function getUpcomingEvents(int $maxEntries = null)
     {
+        $timezone = new DateTimeZone('Europe/Berlin');
         $vcalendar = VObject\Reader::read(self::queryIcsContent());
 
         $events = [];
         $i = 0;
 
-        $now = new DateTime('now', self::TIMEZONE);
+        $now = new DateTime('now', $timezone);
 
         foreach ($vcalendar->VEVENT as $event) {
             // ignore past events
-            if ($event->DTEND->getDateTime(self::TIMEZONE) < $now) {
+            if ($event->DTEND->getDateTime($timezone) < $now) {
                 continue;
             }
 
             $events[] = [
                 'title' => (string) $event->SUMMARY,
-                'datetime' => $event->DTSTART->getDateTime(self::TIMEZONE)->format(DateTimeInterface::ATOM),
+                'datetime' => $event->DTSTART->getDateTime($timezone)->format(DateTimeInterface::ATOM),
                 'location' => (string) $event->LOCATION,
             ];
 
