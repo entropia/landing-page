da4156773856651113ccc3ad50cb1c386cd3772f
Pascal Wittler <git@pascalwittler.de>
2025-03-29 21:05:10 +0100

    Remove additional PHP wrapper function for `file_get_contents()`

--- public/api/entropia-club-status-json-api.php
+++ public/api/entropia-club-status-json-api.php
@@ -5,20 +5,11 @@ class EntropiaClubStatusJsonApi
     const WIKI_SPACE_API_URL = 'https://club.entropia.de/spaceapi';
 
     /**
-     * @param string|null $url the public url of the requested website
-     * @return string the html content of the website
-     */
-    private static function queryJsonContent(string $url = null)
-    {
-        return file_get_contents($url ?? self::WIKI_SPACE_API_URL);
-    }
-
-    /**
      * @return string|false
      */
     private static function requestClubStatus()
     {
-        $space_api_content = self::queryJsonContent();
+        $space_api_content = file_get_contents(self::WIKI_SPACE_API_URL);
         $space_api_json = json_decode($space_api_content, true);
 
         return json_encode(
