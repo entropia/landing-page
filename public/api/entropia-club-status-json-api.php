3579bbcc40929d9c95e0b4501ea1bb73bf6e0cd7
Pascal Wittler <git@pascalwittler.de>
2025-03-29 21:07:38 +0100

    Return/Echo space API response directly via PHP

--- public/api/entropia-club-status-json-api.php
+++ public/api/entropia-club-status-json-api.php
@@ -5,29 +5,14 @@ class EntropiaClubStatusJsonApi
     const WIKI_SPACE_API_URL = 'https://club.entropia.de/spaceapi';
 
     /**
-     * @return string|false
-     */
-    private static function requestClubStatus()
-    {
-        $space_api_content = file_get_contents(self::WIKI_SPACE_API_URL);
-        $space_api_json = json_decode($space_api_content, true);
-
-        return json_encode(
-            [
-                'isOpen' => $space_api_json['state']['open'],
-                'lastChange' => date('c', $space_api_json['state']['lastchange']),
-            ],
-            JSON_UNESCAPED_UNICODE
-        );
-    }
-
-    /**
      * @return void
      */
     public static function main()
     {
+        $space_api_content = file_get_contents(self::WIKI_SPACE_API_URL);
+
         header('Content-type: application/json; charset=utf-8');
-        echo self::requestClubStatus();
+        echo $space_api_content;
     }
 }
 
